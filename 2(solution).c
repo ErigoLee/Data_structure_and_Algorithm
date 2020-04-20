44bd9f49a4be8ac6aaf960658721b1c7ffb859af
ErigoLee <57980181+ErigoLee@users.noreply.github.com>
2020-04-20 20:06:39 +0900

    Create 2(solution).c

---
+++ 2(solution).c
@@ -0,0 +1,16 @@
+//my professor solution
+#include <stdio.h>
+int f(int n){
+  if(n==1) return 1;
+  if(n==2) return 1;
+  if(n==3) return 2;
+  
+  return f(n-1) + f(n-2) + f(n-3);
+}
+
+int main(){
+  int n;
+  scanf("%d",&n);
+  printf("%d\n",f(n));
+  return 0;
+}
